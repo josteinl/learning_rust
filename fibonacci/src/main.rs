aa8a81370578248c0ac32943ff95fbd6c0bd75f0
Jostein Leira <9363084+josteinl@users.noreply.github.com>
2024-12-30 11:06:48 +0100

    Implemented Fibonacci.

--- fibonacci/src/main.rs
+++ fibonacci/src/main.rs
@@ -1,3 +1,24 @@
+use std::io;
+
+fn fibonacci(n: u128) -> u128 {
+    if n == 0 {0}
+    else if n == 1 {1}
+    else { fibonacci(n - 1) + fibonacci(n - 2) }
+}
+
 fn main() {
-    println!("Hello, world!");
+    println!("Let's find the Fibonacci number N:");
+
+    println!("Please enter the number N:");
+    let mut n = String::new();
+
+    io::stdin()
+        .read_line(&mut n)
+        .expect("Failed to read line");
+    let n: u128 = n.trim().parse().expect("Please enter a number!");
+
+    let result= fibonacci(n);
+    println!("The {n}th fibonacci number is: {result}");
+
+
 }
